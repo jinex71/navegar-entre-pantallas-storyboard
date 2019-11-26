fc5d5162eed845a921df35b08959f68ad00214f3
jinex <jeanvelarde2@gmail.com>
2019-11-26 03:38:34 -0500

    Initial Commit

---
+++ proyecto01/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  proyecto01
+//
+//  Created by Jean Carlo on 11/26/19.
+//  Copyright © 2019 Jinex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
